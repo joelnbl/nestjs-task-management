212ab5476d8681d098230b39e602c01854404f26
Joel Bello <joel8bl@gmail.com>
2023-01-05 23:50:54 -0400

    adding services

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,29 @@
+import { Body, Param, Controller, Get, Post, Delete } from '@nestjs/common';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { Task } from './task.model';
+import { TasksService } from './tasks.service';
+
+@Controller('tasks')
+export class TasksController {
+  constructor(private tasksService: TasksService) {}
+
+  @Get()
+  getAllTasks(): Task[] {
+    return this.tasksService.getAllTasks();
+  }
+
+  @Get('/:id')
+  getTaskById(@Param('id') id: string): Task {
+    return this.tasksService.getTaskById(id);
+  }
+
+  @Post()
+  createTask(@Body() createTaskDto: CreateTaskDto): Task {
+    return this.tasksService.createTask(createTaskDto);
+  }
+
+  @Delete('/:id')
+  deletetask(@Param('id') id: string): void {
+    return this.tasksService.deleteTask(id);
+  }
+}
